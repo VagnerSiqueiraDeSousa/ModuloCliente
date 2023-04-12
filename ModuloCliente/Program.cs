2e6454733b0dad03069cc8c980924a941d0033a9
VagnerSousa <107362281+VagnerSiqueiraDeSousa@users.noreply.github.com>
2023-04-12 10:49:00 -0300

    Adicionar arquivos de projeto.

---
+++ ModuloCliente/Program.cs
@@ -0,0 +1,14 @@
+﻿using ModuloCliente;
+
+clientes cliente = new clientes();
+cliente.Nome = "Vagner Sousa";
+cliente.Cpf = "321.654.987-01";
+cliente.Email = "aulaAlura@alura.com";
+cliente.Idade = 39;
+
+Console.WriteLine(cliente.Nome);
+Console.WriteLine(cliente.Cpf);
+Console.WriteLine(cliente.Idade);
+
+Console.WriteLine(cliente.ToString());
+
---
+++ ModuloCliente/clientes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloCliente
+{
+    public class clientes
+    {
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {this.Nome}, \n" +
+                 $"CPF: {this.Cpf}, \n " +
+                 $"E-mail: {this.Email}, \n" +
+                 $"Idade: {this.Idade}";
+
+        }
+    }
+}
